c1345464309ebfcb06869c14f028761b7e4aaf75
jimalaros <48387416+jimalaros@users.noreply.github.com>
2021-07-09 17:12:39 -0500

    Update productos.models.js

--- src/models/productos.models.js
+++ src/models/productos.models.js
@@ -1,42 +1,42 @@
 const productos =  [
     {
         id: 1,
-        articulo: "Hamburguesa",
+        nombre: "Hamburguesa",
         precio: 5000
     },
     {
         id: 2,
-        articulo: "Hamburguesa doble",
+        nombre: "Hamburguesa doble",
         precio: 10500
     },
     {
         id: 3,
-        articulo: "Perro",
+        nombre: "Perro",
         precio: 5500
     },
     {
         id: 4,
-        articulo: "Perro especial",
+        nombre: "Perro especial",
         precio: 9000
     },
     {
         id: 5,
-        articulo: "Coca-cola",
+        nombre: "Coca-cola",
         precio: 3000
     },
     {
         id: 6,
-        articulo: "Sprite",
+        nombre: "Sprite",
         precio: 2500
     },
     {
         id: 7,
-        articulo: "Agua",
+        nombre: "Agua",
         precio: 1500
     },
     {
         id: 8,
-        articulo: "Jugo",
+        nombre: "Jugo",
         precio: 2000
     }
 ]
@@ -47,7 +47,6 @@ const mostrarProductos = () => {
 
 const registrarProductos = (nombre, precio) => {
     const producto = {id: productos.length + 1, nombre, precio}
-    //console.log(producto);
     productos.push(producto);
 }
 
